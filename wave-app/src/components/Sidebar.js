05c20096c5934024819858e26f14ba4a93a24f9a
Fraser Ivison <fraserivison@hotmail.co.uk>
2024-10-28 16:32:13 +0000

    Add Sidebar.css for styling and bootstrap5 links to index.html

--- wave-app/src/components/Sidebar.js
+++ wave-app/src/components/Sidebar.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import './Sidebar.css';
 
 function Sidebar() {
   return (
-    <div>
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
+    <nav className="sidebar bg-dark text-light p-3">
+      <h3 className="text-neon-blue">Wave</h3>
+      <ul className="nav flex-column">
+        <li className="nav-item">
+          <a href="#link1" className="nav-link text-light">Home</a>
+        </li>
+        <li className="nav-item">
+          <a href="#link2" className="nav-link text-light">Discover</a>
+        </li>
+        <li className="nav-item">
+          <a href="#link3" className="nav-link text-light">Projects</a>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
