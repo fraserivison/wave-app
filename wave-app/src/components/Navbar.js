59b3b8c4e9f1479b5af51337df3c3b649708a427
Fraser Ivison <fraserivison@hotmail.co.uk>
2024-10-29 23:17:01 +0000

    Fix button handler and update welcome message logic in Navbar

--- wave-app/src/components/Navbar.js
+++ wave-app/src/components/Navbar.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
+import './Navbar.module.css';
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isSignedIn: false,
+      welcomeMessage: 'Welcome, plesde sign in!'
     };
   }
 
-  handleSignInOut = () => {
+  toggleSignIn = () => {
     this.setState((prevState) => ({
       isSignedIn: !prevState.isSignedIn,
+      welcomeMessage: prevState.isSignedIn ? 'Welcome, please sign in!' : 'Welcome back, DJ!'
     }));
-  };  
+  }; 
 
   render() {
     return (
-      <nav className="navbar bg-dark text-light p-3">
-        <h3 className="navbar-logo">Logo Here</h3>
-        <p className="navbar-welcome">
-          {this.state.isSignedIn ? 'Welcome back!' : 'Welcome, please sign in'}
-        </p>
-        <button className="btn btn-outline-light" onClick={this.handleSignInOut}>
-          {this.state.isSignedIn ? 'Sign Out' : 'Sign In'}
-        </button>
+      <nav className="navbar bg-dark text-light">
+        <div className="container-fluid">
+            <span className="navbar-brand">My Logo</span>
+            <button className="btn btn-outline-light" onClick={this.toggleSignIn}>
+                {this.state.isSignedIn ? 'Sign Out' : 'Sign In'} 
+            </button>
+            <span className="navbar-text">
+            {this.state.welcomeMessage}
+          </span>
+        </div>
       </nav>
     );
   }
