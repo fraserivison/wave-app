079fece0e288c8aa3eee127e25d4c27f0b8e8c50
Fraser Ivison <fraserivison@hotmail.co.uk>
2024-12-24 12:51:47 +0000

    Update sidebar styling, improve layout, hover effects, and icon alignment

--- wave-app/src/components/Sidebar/Sidebar.js
+++ wave-app/src/components/Sidebar/Sidebar.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
-import { FaArrowRight, FaArrowLeft } from 'react-icons/fa';
-import styles from './Sidebar.module.css';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import {
+  FaArrowRight,
+  FaArrowLeft,
+  FaHome,
+  FaUser,
+  FaSearch,
+  FaQuestion,
+} from "react-icons/fa";
+import styles from "./Sidebar.module.css";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -11,30 +18,39 @@ function Sidebar() {
   };
 
   return (
-    <nav className={`${styles.sidebar} ${isExpanded ? styles.expanded : ''}`}>
-  <h3 onClick={toggleSidebar} className={styles.sidebarToggle}>
-    {isExpanded ? <FaArrowLeft /> : <FaArrowRight />}
-  </h3>
-  <ul className="nav flex-column">
-    <li className="nav-item">
-      <Link to="/" className="nav-link text-light">Home</Link>
-    </li>
-    <li className="nav-item">
-      <Link to="/profile" className="nav-link text-light">Profile</Link>
-    </li>
-    <li className="nav-item">
-      <Link to="/discover" className="nav-link text-light">Discover</Link>
-    </li>
-    <li className="nav-item">
-      <Link to="/support" className="nav-link text-light">Support</Link>
-    </li>
-  </ul>
-</nav>
-
+    <nav className={`${styles.sidebar} ${isExpanded ? styles.expanded : ""}`}>
+      <h3 onClick={toggleSidebar} className={styles.sidebarToggle}>
+        {isExpanded ? <FaArrowLeft /> : <FaArrowRight />}
+      </h3>
+      <ul className="nav flex-column gap-4">
+        {" "}
+        <li className="nav-item">
+          <Link to="/" className="nav-link text-light">
+            <FaHome className="icon" style={{ color: "#00ffcc" }} />
+            {isExpanded && " Home"}
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/profile" className="nav-link text-light">
+            <FaUser className="icon" style={{ color: "#00ffcc" }} />
+            {isExpanded && " Profile"}
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/discover" className="nav-link text-light">
+            <FaSearch className="icon" style={{ color: "#00ffcc" }} />
+            {isExpanded && " Discover"}
+          </Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/support" className="nav-link text-light">
+            <FaQuestion className="icon" style={{ color: "#00ffcc" }} />
+            {isExpanded && " Support"}
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
 export default Sidebar;
-
-
-
