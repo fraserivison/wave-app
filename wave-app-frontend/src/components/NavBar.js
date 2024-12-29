1211a79870c76aaa4e4b39f05b4d20a75f9bd0df
fraserivison <fraserivison@hotmail.co.uk>
2024-12-29 16:52:46 +0000

    Set up routing for homepage, sign-in, and sign-up pages in the navbar

--- wave-app-frontend/src/components/NavBar.js
+++ wave-app-frontend/src/components/NavBar.js
@@ -1,25 +1,49 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-    return (
-    <Navbar expand="md" fixed="top" className="bg-body-tertiary">
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-        <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto text-left">
-            <Nav.Link><i className="fa-solid fa-house-chimney-window"></i>Home</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-right-to-bracket"></i>Sign In</Nav.Link>
-            <Nav.Link><i className="fa-solid fa-user-plus"></i>Sign Up</Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fa-solid fa-house-chimney-window"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fa-solid fa-right-to-bracket"></i>Sign In
+            </NavLink>
+            <NavLink
+              to="/signup"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fa-solid fa-user-plus"></i>Sign Up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
 export default NavBar;
+
