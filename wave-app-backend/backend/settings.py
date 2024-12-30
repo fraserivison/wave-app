7f1f24d4d7bd6e3b7abecea5b2515e09d6e164e3
fraserivison <fraserivison@hotmail.co.uk>
2024-12-30 12:34:26 +0000

    Create Procfile and prepare for Heroku deployment

--- wave-app-backend/backend/settings.py
+++ wave-app-backend/backend/settings.py
@@ -3,10 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-1(se90@#nr)_ad32l6g8*^dgl6#*rxlq+5r1qk0a2qfb-=$n=$'
 
@@ -21,6 +17,11 @@ ALLOWED_HOSTS = [
 
 CSRF_TRUSTED_ORIGINS = [
     'https://8000-fraserivison-waveapp-g8rmuaifqj9.ws-eu117.gitpod.io',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://8080-fraserivison-waveapp-g8rmuaifqj9.ws-eu117.gitpod.io",
+    "https://wave-react-frontend-e9f1c747c897.herokuapp.com",
 ]
 
 INSTALLED_APPS = [
