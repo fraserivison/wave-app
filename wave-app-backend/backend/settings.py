a07f7596b4cdbe7bcaeb12240cacfc4738a86464
fraserivison <fraserivison@hotmail.co.uk>
2025-01-01 08:32:15 +0000

    Confirm static files are set up correctly on settings.py

--- wave-app-backend/backend/settings.py
+++ wave-app-backend/backend/settings.py
@@ -119,6 +119,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
