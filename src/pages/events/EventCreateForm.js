2eb9fd30bef92b696667fc076cd17826927754c1
fraserivison <fraserivison@hotmail.co.uk>
2025-01-19 08:54:17 +0000

    Create basic Event page and write logic to reach out to the API

--- src/pages/events/EventCreateForm.js
+++ src/pages/events/EventCreateForm.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Alert from "react-bootstrap/Alert";
 
--- src/pages/events/EventPage.js
+++ src/pages/events/EventPage.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
+
 import appStyles from "../../App.module.css";
-import eventStyles from "../../Event.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
@@ -18,6 +19,7 @@ function EventPage() {
           axiosReq.get(`/events/${id}`),
         ]);
         setEvent({ results: [event] });
+        console.log(event);
       } catch (err) {
         console.log(err);
       }
@@ -28,29 +30,19 @@ function EventPage() {
 
   return (
     <Row className="h-100">
-      <Col className={`${eventStyles.eventPageContainer} py-2 p-0 p-lg-2`} lg={8}>
-        <div className={eventStyles.eventContent}>
-          <h2 className={eventStyles.eventTitle}>{event.results[0]?.title}</h2>
-          <p className={eventStyles.eventDate}>{event.results[0]?.date}</p>
-          <img
-            src={event.results[0]?.image}
-            alt={event.results[0]?.title}
-            className={eventStyles.eventImage}
-          />
-          <p className={eventStyles.eventDescription}>
-            {event.results[0]?.description}
-          </p>
-        </div>
-        <Container className={appStyles.Content}>
-          <div className={eventStyles.eventComments}>Comments</div>
-        </Container>
+      <Col className="py-2 p-0 p-lg-2" lg={8}>
+        <p>Popular profiles for mobile</p>
+        <p>Event component</p>
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        {/* Sidebar or other content can go here */}
+        Popular profiles for desktop
       </Col>
     </Row>
   );
 }
 
 export default EventPage;
+
+
 
