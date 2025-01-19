239ed562970b53b5fb1e0a1c765e1ffd15639b6a
fraserivison <fraserivison@hotmail.co.uk>
2025-01-19 10:25:21 +0000

    Redirect root URL (/) to /discover to make TracksPage the main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom"; // Import Redirect
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
@@ -9,7 +9,7 @@ import EventCreateForm from "./pages/events/EventCreateForm";
 import EventPage from "./pages/events/EventPage";
 import TrackCreateForm from "./pages/tracks/TrackCreateForm";
 import TrackPage from "./pages/tracks/TrackPage";
-import TracksPage from "./pages/tracks/TracksPage";
+import TracksPage from "./pages/tracks/TracksPage"; // TracksPage is the discovery page
 import TrackEditForm from "./pages/tracks/TrackEditForm";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
@@ -22,6 +22,9 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
+          {/* Redirect root URL to /discover */}
+          <Route exact path="/" render={() => <Redirect to="/discover" />} />
+
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/tracks/create" render={() => <TrackCreateForm />} />
@@ -29,7 +32,10 @@ function App() {
           <Route exact path="/tracks/:id" render={() => <TrackPage />} />
           <Route exact path="/events/:id" render={() => <EventPage />} />
           <Route exact path="/tracks/:id/edit" render={() => <TrackEditForm />} />
+          
+          {/* TracksPage (discovery page) will now be the main route */}
           <Route path="/discover" component={TracksPage} />
+          
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
@@ -38,3 +44,4 @@ function App() {
 }
 
 export default App;
+
