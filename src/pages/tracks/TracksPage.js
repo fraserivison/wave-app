09e421b505431cf9486fe2dceb61ac93908c9128
fraserivison <fraserivison@hotmail.co.uk>
2025-01-19 03:26:13 +0000

    Implement search functionality for the TracksPage component and add infinite scrolling

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -14,16 +14,19 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function TracksPage({ message, filter = "" }) {
   const [tracks, setTracks] = useState({ results: [] });
   const [hasLoaded, setHasLoaded] = useState(false);
+  const [query, setQuery] = useState("");
   const { pathname } = useLocation();
 
   useEffect(() => {
     const fetchTracks = async () => {
       try {
-        const { data } = await axiosReq.get(`/tracks/?${filter}`);
+        const { data } = await axiosReq.get(`/tracks/?${filter}search=${query}`);
         setTracks(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,19 +35,45 @@ function TracksPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchTracks();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchTracks();
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search tracks"
+          />
+        </Form>
+
         {hasLoaded ? (
           <>
             {tracks.results.length ? (
-              tracks.results.map((track) => (
-                <Track key={track.id} {...track} setTracks={setTracks} />
-              ))
+              <InfiniteScroll
+                children={tracks.results.map((track) => (
+                  <Track key={track.id} {...track} setTracks={setTracks} />
+                ))}
+                dataLength={tracks.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!tracks.next}
+                next={() => fetchMoreData(tracks, setTracks)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
@@ -65,3 +94,4 @@ function TracksPage({ message, filter = "" }) {
 }
 
 export default TracksPage;
+
