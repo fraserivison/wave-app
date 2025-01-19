c7a170610323cb82d64f21859105a56eca9cb3d5
fraserivison <fraserivison@hotmail.co.uk>
2025-01-19 02:35:28 +0000

    Implement logic for TracksPage component to fetch and display filtered posts. Add loading spinner, no results message, and asset display, and enable post mapping with like/unlike functionality and dynamic updates based on route changes

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Track from "./Track";
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/TracksPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function TracksPage() {
-  
+import NoResults from "../../assets/no-results.png";
+
+function TracksPage({ message, filter = "" }) {
+  const [tracks, setTracks] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchTracks = async () => {
+      try {
+        const { data } = await axiosReq.get(`/tracks/?${filter}`);
+        setTracks(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchTracks();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+          <>
+            {tracks.results.length ? (
+              tracks.results.map((track) => (
+                <Track key={track.id} {...track} setTracks={setTracks} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
