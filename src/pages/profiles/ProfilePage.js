dd94e37997fcf31b363e7be6feaae0a1b2f0947e
fraserivison <fraserivison@hotmail.co.uk>
2025-01-21 20:03:30 +0000

    Add styling for profile page

--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { axiosRes } from "../../api/axiosDefaults";
-import Avatar from "../../components/Avatar";
+import { Media } from "react-bootstrap";
 import styles from "../../styles/ProfilePage.module.css";
 import Profile from "./Profile";
 
@@ -27,34 +27,44 @@ const ProfilePage = () => {
   }
 
   return (
-    <div className={styles.ProfilePage}>
-      <div className="text-center">
-        <Avatar src={profile.image} height={100} />
-        <h2>{profile.dj_name}</h2>
-        <p>{profile.bio}</p>
-      </div>
-      <div className="text-center">
-        <Link to={`/profiles/${id}/edit`} className="btn btn-primary">
-          Edit Profile
-        </Link>
-      </div>
-      <div className={styles.Details}>
-        <p>
-          <strong>Joined:</strong> {new Date(profile.created_at).toLocaleDateString()}
-        </p>
-        <p>
-          <strong>Last Updated:</strong> {new Date(profile.updated_at).toLocaleDateString()}
-        </p>
-      </div>
+    <div className={styles.ProfileContainerRow}>
+      <div className={styles.ProfileCol}>
+        <div className={styles.ProfileContainer}>
+          {/* Profile Image */}
+          <div
+            className={styles.ProfileImage}
+            style={{ backgroundImage: `url(${profile.image})` }}
+          >
+            <div className={styles.ProfileHeader}>
+              <Media className="d-flex align-items-center justify-content-between">
+                <div className={styles.ProfileName}>
+                  <h2>{profile.dj_name}</h2>
+                </div>
+                <div className={styles.ProfileTitleWrapper}>
+                </div>
+              </Media>
+            </div>
+          </div>
 
-      {/* Profile Section */}
+          {/* Edit Profile */}
+          <div className={styles.ProfileCenter}>
+            <Link to={`/profiles/${id}/edit`} className={styles.EditProfileButton}>
+              Edit Profile
+            </Link>
+          </div>
+
+          {/* Footer Section */}
+          <div className={styles.ProfileFooter}>
+            <div className={styles.ProfileOwner}>
+              <strong>Joined:</strong> {new Date(profile.created_at).toLocaleDateString()}
+            </div>
+            <p className={styles.Bio}>{profile.bio}</p>
+          </div>
+        </div>
+      </div>
       <Profile profile={profile} />
-
-      
     </div>
   );
 };
 
 export default ProfilePage;
-
-
