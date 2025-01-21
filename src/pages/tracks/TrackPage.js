d7beb576154bba82cb6d4ed8ab7a041e0c4cc756
fraserivison <fraserivison@hotmail.co.uk>
2025-01-21 15:17:48 +0000

    Remove column from TrackPage to allow element to stretch across the screen

--- src/pages/tracks/TrackPage.js
+++ src/pages/tracks/TrackPage.js
@@ -1,14 +1,8 @@
 import React, { useEffect, useState } from "react";
-
-import Col from "react-bootstrap/Col";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-
-import appStyles from "../../App.module.css";
+import { Col, Row } from "react-bootstrap"; // Make sure Col is imported
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Track from "./Track";
-
 
 function TrackPage() {
   const { id } = useParams();
@@ -32,14 +26,13 @@ function TrackPage() {
 
   return (
     <Row className="h-100">
-      <Col className="py-2 p-0 p-lg-2" lg={8}>
-      <h1>Track Preview</h1>
+      <Col className="py-2 p-0 p-lg-2" lg={12}>
+        <h1>Track Preview</h1>
         <Track {...track.results[0]} setTracks={setTrack} trackPage />
-      </Col>
-      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
       </Col>
     </Row>
   );
 }
 
 export default TrackPage;
+
