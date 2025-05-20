a2d0c1f8f405add25123e402aefc41bc3ce84264
Fraser <fraserivison@hotmail.co.uk>
2025-05-20 16:36:17 +0100

    Adjust layout for better user experience- tracks to fill whole page

--- src/pages/tracks/TracksPage.js
+++ src/pages/tracks/TracksPage.js
@@ -30,7 +30,9 @@ function TracksPage({ message, filter = "" }) {
   useEffect(() => {
     const fetchTracks = async () => {
       try {
-        const { data } = await axiosReq.get(`/tracks/?${filter}search=${query}`);
+        const { data } = await axiosReq.get(
+          `/tracks/?${filter}search=${query}`
+        );
         setTracks(data);
         setHasLoaded(true);
       } catch (err) {
@@ -49,59 +51,102 @@ function TracksPage({ message, filter = "" }) {
   }, [filter, query, pathname]);
 
   return (
-    <Row className={`h-100 ${styles.TracksPageContainer}`}>
-      <Col xs={12} lg={8} className="py-2 p-0 p-lg-2">
-        <h1>Discover</h1>
-        <p className={styles.infoBox}>
-          Discover, rate and{" "}
-          <Link to="/tracks/create" className={styles.link}>share</Link>{" "}
-          your music with the community.
-        </p>
-        <i className={`fas fa-search ${styles.SearchIcon}`} />
-        <Form className={styles.SearchBar} onSubmit={(e) => e.preventDefault()}>
-          <Form.Control
-            value={query}
-            onChange={(e) => setQuery(e.target.value)}
-            type="text"
-            placeholder="Search tracks, genre, artist..."
-          />
-        </Form>
+    <Container fluid className={styles.pageWrapper}>
+      {/* Header */}
+      <Row>
+        <Col>
+          <h1>Discover</h1>
+          <p className={styles.infoBox}>
+            Discover, rate and{" "}
+            <Link to="/tracks/create" className={styles.link}>
+              share
+            </Link>{" "}
+            your music with the community.
+          </p>
+        </Col>
+      </Row>
 
-        {hasLoaded ? (
-          tracks.results.length ? (
-            <InfiniteScroll
-              dataLength={tracks.results.length}
-              loader={<Asset spinner />}
-              hasMore={!!tracks.next}
-              next={() => fetchMoreData(tracks, setTracks)}
-            >
-              <Row className={styles.TrackContainerRow}>
-                {tracks.results.map((track) => (
-                  <Col key={track.id} xs={6} sm={6} md={4} lg={4}>
-                    <Track {...track} setTracks={setTracks} />
-                  </Col>
-                ))}
-              </Row>
-            </InfiniteScroll>
+      {/* Info + Sidebar Section */}
+      <Row className="mt-3">
+        {/* Trending Genres */}
+        <Col xs={12} md={4}>
+          <div className={styles.infoCard}>
+            <h5>Explore Trending Genres</h5>
+            <p>
+              Stay updated with the latest sounds in hip-hop, lo-fi,
+              synthwave, and more.
+            </p>
+          </div>
+        </Col>
+
+        {/* Connect with Artists */}
+        <Col xs={12} md={4}>
+          <div className={styles.infoCard}>
+            <h5>Connect with Artists</h5>
+            <p>
+              Rate tracks, leave feedback, and build your network in the music
+              community.
+            </p>
+          </div>
+        </Col>
+
+        {/* Sidebar */}
+        <Col xs={12} md={4}>
+          <TopTracks />
+          <ArtistSpotlight />
+          <StatsSection />
+        </Col>
+      </Row>
+
+      {/* Testimonials */}
+      <Row className="mt-4">
+        <Col>
+          <Testimonials />
+        </Col>
+      </Row>
+
+      {/* Search + Track Results */}
+      <Row className="mt-4">
+        <Col>
+          <i className={`fas fa-search ${styles.SearchIcon}`} />
+          <Form className={styles.SearchBar} onSubmit={(e) => e.preventDefault()}>
+            <Form.Control
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+              type="text"
+              placeholder="Search tracks, genre, artist..."
+            />
+          </Form>
+
+          {hasLoaded ? (
+            tracks.results.length ? (
+              <InfiniteScroll
+                dataLength={tracks.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!tracks.next}
+                next={() => fetchMoreData(tracks, setTracks)}
+              >
+                <Row className={styles.TrackContainerRow}>
+                  {tracks.results.map((track) => (
+                    <Col key={track.id} xs={12} sm={6} md={4} lg={3}>
+                      <Track {...track} setTracks={setTracks} />
+                    </Col>
+                  ))}
+                </Row>
+              </InfiniteScroll>
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )
           ) : (
             <Container className={appStyles.Content}>
-              <Asset src={NoResults} message={message} />
+              <Asset spinner />
             </Container>
-          )
-        ) : (
-          <Container className={appStyles.Content}>
-            <Asset spinner />
-          </Container>
-        )}
-      </Col>
-
-      <Col xs={12} lg={4} className="py-2 p-0 p-lg-2">
-        <TopTracks />
-        <ArtistSpotlight />
-        <StatsSection />
-        <Testimonials />
-      </Col>
-    </Row>
+          )}
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
