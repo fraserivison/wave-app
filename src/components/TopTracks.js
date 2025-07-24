edf779b4e44f0e7da3db22a874ea7872fb07dcf5
Fraser <fraserivison@hotmail.co.uk>
2025-07-24 06:39:43 +0100

    Add border to tracknumber, space out title and text and add padding to give toptrack card more space

--- src/components/TopTracks.js
+++ src/components/TopTracks.js
@@ -3,15 +3,15 @@ import styles from "../styles/TopTracks.module.css";
 
 const TopTracks = () => {
   const dummyTracks = [
-    { id: 1, title: "Sunset twice a day", artist: "Lionel Mellor" },
-    { id: 2, title: "Midnight Walker", artist: "Nova" },
-    { id: 3, title: "Low Tide Vibes", artist: "Andy Lumen" },
-    { id: 4, title: "Come get it", artist: "McArthur" },
-    { id: 5, title: "Out of the ocean", artist: "Ryan McCurrie" },
-    { id: 6, title: "Dancing on my own", artist: "Paul Beckingfield" },
+    { id: 1, title: "Twice a day", artist: "Lionel Mellor" },
+    { id: 2, title: "Midnight Man", artist: "Nova" },
+    { id: 3, title: "Reel Vibes", artist: "Andy Lumen" },
+    { id: 4, title: "Come get it!", artist: "McArthur" },
+    { id: 5, title: "H2o Dweller", artist: "Ryan McCurrie" },
+    { id: 6, title: "Dancing Clown", artist: "Paul Beckingfield" },
     { id: 7, title: "Tailor Swifty", artist: "DJ Bob Clarke" },
     { id: 8, title: "Business Trip", artist: "Duski" },
-    { id: 9, title: "Three's a crowd", artist: "Phillipe Noir" },
+    { id: 9, title: "3's a crowd", artist: "Phillipe Noir" },
     { id: 10, title: "Obsidian", artist: "Teddy Tex" },
   ];
 
@@ -22,7 +22,9 @@ const TopTracks = () => {
         <div className={styles.column}>
           {dummyTracks.slice(0, 5).map((track, index) => (
             <div key={track.id} className={styles.trackWrapper}>
-              <div className={styles.trackNumber}>{index + 1}</div>
+              <div className={styles.trackNumberWrapper}>
+                <div className={styles.trackNumber}>{index + 1}</div>
+              </div>
               <div className={styles.trackCard}>
                 <div className={styles.trackTitle}>{track.title}</div>
                 <div className={styles.trackArtist}>
@@ -36,7 +38,9 @@ const TopTracks = () => {
         <div className={styles.column}>
           {dummyTracks.slice(5).map((track, index) => (
             <div key={track.id} className={styles.trackWrapper}>
-              <div className={styles.trackNumber}>{index + 6}</div>
+              <div className={styles.trackNumberWrapper}>
+                <div className={styles.trackNumber}>{index + 6}</div>
+              </div>
               <div className={styles.trackCard}>
                 <div className={styles.trackTitle}>{track.title}</div>
                 <div className={styles.trackArtist}>
