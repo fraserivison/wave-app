d4f6b0eb09f75e6abfd75ad73528a195f8e3719c
Fraser <fraserivison@hotmail.co.uk>
2025-09-07 21:06:18 +0100

    Add more text to even out testimonial boxes

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,8 +5,8 @@ const Testimonials = () => {
   const feedback = [
     { id: 1, name: "Alex North", quote: "It's helped me discover new artists to use for my mixes- gamechanger!" },
     { id: 2, name: "Jamie Jacksyn", quote: "Uploading my music here was the best move to expose me to the world." },
-    { id: 3, name: "DJ Sammi", quote: "Love the community here, it's for electric music lovers!" },
-    { id: 4, name: "Taylor Made", quote: "I can't believe how easy it is to find new tracks here." },
+    { id: 3, name: "DJ Sammi", quote: "Love the community here, it's for electric music lovers- what you waiting for!" },
+  { id: 4, name: "Taylor Made", quote: "I can't believe how easy it is to find new tracks here. Some absolute belters!!" },
     { id: 5, name: "Jordi", quote: "Collaborating with artists here has been amazing! BIG things coming soon!" },
     { id: 6, name: "Prysm", quote: "I love how interactive and welcoming the community is, love it!." },
   ];
