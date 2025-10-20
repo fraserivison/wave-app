f29e6df995856210a8b43f822b687e9e9c30c3e5
Fraser <fraserivison@hotmail.co.uk>
2025-10-20 22:57:33 +0100

    Change axios defaults back to original

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,36 +1,8 @@
 import axios from "axios";
 
-// Base URL for deployed backend
-const baseURL = "https://wave-drf-api-1157a4fa181b.herokuapp.com";
+axios.defaults.baseURL = "https://wave-drf-api-1157a4fa181b.herokuapp.com";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
 
-// Helper to read cookies
-function getCookie(name) {
-  const cookieValue = document.cookie
-    .split("; ")
-    .find((row) => row.startsWith(name + "="));
-  return cookieValue ? decodeURIComponent(cookieValue.split("=")[1]) : null;
-}
-
-// Axios instance for requests that send data
-export const axiosReq = axios.create({
-  baseURL: baseURL,
-  withCredentials: true,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-// Axios instance for responses (could be used for read-only requests)
-export const axiosRes = axios.create({
-  baseURL: baseURL,
-  withCredentials: true,
-});
-
-// Add CSRF token to axiosReq for unsafe methods
-axiosReq.interceptors.request.use((config) => {
-  const csrfToken = getCookie("csrftoken");
-  if (csrfToken && ["post", "put", "patch", "delete"].includes(config.method)) {
-    config.headers["X-CSRFToken"] = csrfToken;
-  }
-  return config;
-});
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
